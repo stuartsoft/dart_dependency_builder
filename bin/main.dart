245de7b7fc7e8f10f6e7a8d4f7cd39cc05bfb75b
Stuart Bowman <stuartsoft2012@gmail.com>
2020-02-29 16:02:34 -0500

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dependency_builder/dart_dependency_builder.dart' as dart_dependency_builder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_dependency_builder.calculate()}!');
+}
---
+++ test/dart_dependency_builder_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_dependency_builder/dart_dependency_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
